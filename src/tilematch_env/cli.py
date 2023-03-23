55eed3d119dda219fbe5ca7fa70c8649301f33fa
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-23 09:56:28 -0700

    Update to game factory interface to launch games

--- src/tilematch_env/cli.py
+++ src/tilematch_env/cli.py
@@ -7,10 +7,15 @@
 import click
 
 from tilematch_tools import GameEngine
-from columns_widget.cli import columns_init
+from columns_widget import ColumnsGameFactory
 
 @click.command()
 def tmge():
     """Entry point to tmge"""
-    ge = GameEngine([columns_init(), columns_init()])
+    ge = GameEngine(
+            [
+                ColumnsGameFactory.create_game(),
+                ColumnsGameFactory.create_game()
+                ]
+    )
     ge.run()
