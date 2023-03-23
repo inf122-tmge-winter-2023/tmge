1b069eafa810d69308c3be6c8ed416755c787011
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-22 22:18:30 -0700

    Add logging facilities

--- src/tilematch_env/__init__.py
+++ src/tilematch_env/__init__.py
@@ -4,3 +4,14 @@
     :module_author: Nathan Mendoza (nathancm@uci.edu)
 """
 
+import logging
+
+LOGGER = logging.getLogger(__name__)
+LOG_HANDLER = logging.StreamHandler()
+LOG_FORMAT = logging.Formatter('[%(asctime)s|%(name)s|%(levelname)s] - %(message)s')
+
+LOGGER.setLevel(logging.INFO)
+LOG_HANDLER.setLevel(logging.INFO)
+
+LOG_HANDLER.setFormatter(LOG_FORMAT)
+LOGGER.addHandler(LOG_HANDLER)
